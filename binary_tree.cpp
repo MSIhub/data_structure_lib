232d086eb65cc76d43654955b24e3c2efe2b5e9e
sadiq <sadiq.ikbal@gmail.com>
2022-06-28 19:01:34 +0200

    printtabs feature added\n -for better visibility

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -19,24 +19,41 @@ class treenode
         
 };
 
+void printtabs(int numtabs)
+{
+    for(int i = 0; i < numtabs; i++)
+        printf("\t");
+}
 
 template<class T> 
-void printtree(treenode<T>* tn)
+void printtree_rec(treenode<T>* tn, int level)
 {
     if(tn == nullptr)
     {
+        printtabs(level);
         std::cout << "------\n";
         return;
     }
     //PREORDER TRAVERSAL
+    printtabs(level);
     printf("value: %d\n", tn->value());
+    printtabs(level);
     printf("left\n");
-    printtree(tn->m_left);
+
+    printtree_rec(tn->m_left, level+1);
+    printtabs(level);
     printf("right\n");
-    printtree(tn->m_right);
+
+    printtree_rec(tn->m_right, level+1);
+    printtabs(level);
     printf("done\n");
 }
 
+template<class T> 
+void printtree(treenode<T>* root)
+{
+    printtree_rec(root, 0);
+}
 
 int main()
 {
